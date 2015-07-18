a2d85e646101450545770c86785e4d2995352e07
Chris Denniston <octopuscabbage@gmail.com>
2015-07-18 16:38:01 -0500

    fitbark backend done

--- backend/fitbark.py
+++ backend/fitbark.py
@@ -1,14 +1,69 @@
 import requests
 import json
+from util import *
 
-class endpoints:
-    dog_info = "http://app.fitbark.com/api/dog/" 
-    dog_goal = "https://app.fitbark.com/api/v1/daily_goal/"
-    dog_picture = "https://app.fitbark.com/api/v1/picture/dog/"
+class Dog:
+    def __info__(self,name,id):
+        self.name = name
+        self.id = id
+        self.log = []
+    def add_log(self,entry):
+        self.log.append(entry)
     
-def make_get_request(token,endpoint,dog_id,json_response=True):
+class LogEntry:
+    def __info__(self,date,activity,target):
+       self.date = date
+       self.activity = activity
+       self.target = target
+
+test_token = "29deef75ecf5b43d012d05bec21b43acba0c20215350930e9ac9890e966d5ceb"
+test_dog = 1727
+
+def get_dog_data(token):
+    ''''Gets Dog Objects based on a users token'''
+    dogs = get_related_dogs(token)
+    dog_objs = map(lambda d: {'name': d['dog']['name'],'id':d['dog']['id'] },dogs['dog_relations'])
+    for dog in dog_objs:
+       activity_series = get_activity_series(token,dog['id']) 
+       if('status' in activity_series):
+           continue
+       dog['log'] = []
+       for entry in activity_series['records']:
+           dog['log'].append({'date' : entry['date'], 'activity' : entry['activity_value'], 'target' : entry['daily_target']})
+       
+    return dog_objs
+        
+    
+class endpoints:
+    base =  "http://app.fitbark.com/api/v1/"
+    dog_info = base + "dog/" 
+    dog_goal = base + "daily_goal/"
+    dog_picture = base + "picture/dog/"
+    related_dogs = base + "dog_relations"
+    activity_log = base + "activity_series"
+    
+def get_activity_series(token,dog_id):
+    post_data = __make_post_data(dog_id)
     header = make_header(token)
-    response = requests.get(endpoint + str(dog_id),headers=payload)
+    header["Content-Type"] = "application/json"
+    response = requests.post(endpoints.activity_log,headers=header,data=post_data)
+    return __guarded_json_load(response.text)
+    
+def __make_post_data(id):
+    return json.dumps({"activity_series": {"dog_id": str(id),"from": str(n_months_ago(1)), "to" : str(current_date()),'resolution':'HOURLY' }} )
+    
+
+def __guarded_json_load(text):
+    if(text != u' '):
+        return json.loads(text)
+    else:
+        return bad_status()
+
+def bad_status():
+    return json.dumps({"status":"BAD"})
+
+def make_get_request(token,endpoint,dog_id,json_response=True):
+    response = requests.get(endpoint + str(dog_id),headers=make_header(token))
     if (json_response):
         return json.loads(response.text)
     else:
@@ -17,17 +72,16 @@ def make_get_request(token,endpoint,dog_id,json_response=True):
 def make_header(bearer_token):
     return {"Authorization": "Bearer " + bearer_token, "Accept": "application/json"}
 
-
 def get_dog_info(bearer_token,dog_id):
     header = make_header(bearer_token)
-    response = requests.get(dog_info + str(dog_id), headers=payload)
+    response = requests.get(endpoints.dog_info + str(dog_id), headers=make_header(bearer_token))
     out = json.loads(response.text)
     return out
 
 def get_activity(bearer_token,dog_id):
     header = make_header(bearer_token)
     dog_activity = {}
-    response = requests.get(dog_info + str(dog_id), headers=payload)
+    response = requests.get(endpoints.dog_info + str(dog_id), headers=make_header(bearer_token))
     out = json.loads(response.text)
     name = out["dog"]["name"]
     activity = out["dog"]["activity_value"]
@@ -50,3 +104,6 @@ def get_daily_goal(bearer_token,dog_id):
 def get_dog_picture(bearer_token,dog_id):
     return make_get_request(bearer_token,endpoints.dog_picture,dog_id)
     
+
+def get_related_dogs(bearer_token):
+    return make_get_request(bearer_token,endpoints.related_dogs,"")
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import os
 from flask.ext.api import FlaskAPI
-import fitbark
+from fitbark import get_dog_data
 
 app = FlaskAPI(__name__)
 
@@ -12,30 +12,7 @@ def hello():
 def example():
     return {"yo":"it works"}
 
-class fitbark_endpoints:
-    fitbark_base = "/fitbark"
-    auth_and_id = "/<key>/<int:id>/"
-
-    #actually usable ones
-    dog_picture = fitbark_base + "/dog_picture" + auth_and_id
-    dog_info = fitbark_base + "/dog_info" + auth_and_id
-    dog_activity = fitbark_base + "/dog_activity" + auth_and_id
-    dog_goal = fitbark_base + "/daily_goal" + auth_and_id
-
-
-@app.route(fitbark_endpoints.dog_picture)
-def dog_picture(key,id):
-    return get_dog_picture(key,id)
-
-@app.route(fitbark_endpoints.dog_info)
-def dog_info(key,id):
-    return get_dog_info(key,id)
-
-@app.route(fitbark_endpoints.dog_activity)
-def dog_activity(key,id):
-    return get_dog_activity(key,id)
-
-@app.route(fitbark_endpoints.dog_goal)
-def dog_goal(key,id):
-    return get_dog_goal(key,id)
+@app.route("/fitbark/<token>")
+def dog_data(token):
+    return get_dog_data(token)
 
