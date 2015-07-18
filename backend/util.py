1f687ced4bf833800e0ba4a2b58707e7eb479ca4
Chris Denniston <octopuscabbage@gmail.com>
2015-07-18 15:42:14 -0500

    added datetime utility functions

---
+++ backend/util.py
@@ -0,0 +1,14 @@
+import datetime
+
+def current_date():
+    return (datetime.datetime.now().date())
+    
+def n_months_ago(n):
+    return monthdelta(datetime.datetime.now(),-n).date()
+
+def monthdelta(date, delta):
+    m, y = (date.month+delta) % 12, date.year + ((date.month)+delta-1) // 12
+    if not m: m = 12
+    d = min(date.day, [31,
+       29 if y%4==0 and not y%400==0 else 28,31,30,31,30,31,31,30,31,30,31][m-1])
+    return date.replace(day=d,month=m, year=y)
